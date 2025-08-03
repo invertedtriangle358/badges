6f36e7a5b30ebc4c02b9258c8c19e965f0cc6b41
invertedtriangle358 <inverted.triangle358@gmail.com>
2025-08-03 23:07:31 +0900

    title translate japanese

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Badges</title>
+        <title>バッジ</title>
       </Helmet>
       <Layout>
         <SignUp />
